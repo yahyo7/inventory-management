38a8608e89df7de4e2c9bd6e07180ae0906ae257
Seven <ahmadaliev.sd@gmail.com>
2024-08-09 16:36:29 +0900

    dashboard api created

---
+++ server/dist/src/index.js
@@ -0,0 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
+const morgan_1 = __importDefault(require("morgan"));
+// Route imports
+const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
+// configurations
+dotenv_1.default.config();
+// Create an instance of an Express application
+const app = (0, express_1.default)();
+// Middleware to parse incoming JSON request bodies
+app.use(express_1.default.json());
+// Add security headers to responses using Helmet
+app.use((0, helmet_1.default)());
+// Configure Helmet to set Cross-Origin Resource Policy header
+app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// Log HTTP requests with Morgan in 'common' format
+app.use((0, morgan_1.default)("common"));
+// Parse incoming JSON request bodies
+app.use(body_parser_1.default.json());
+// Parse URL-encoded request bodies (typically from HTML forms)
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+// Enable Cross-Origin Resource Sharing (CORS) to allow requests from different origins
+app.use((0, cors_1.default)());
+// routes
+app.use("/dashboard", dashboardRoutes_1.default);
+// server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ server/src/index.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+// Route imports
+import dashboardRoutes from "./routes/dashboardRoutes";
+
+// configurations
+dotenv.config();
+// Create an instance of an Express application
+const app = express();
+
+// Middleware to parse incoming JSON request bodies
+app.use(express.json());
+
+// Add security headers to responses using Helmet
+app.use(helmet());
+
+// Configure Helmet to set Cross-Origin Resource Policy header
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+// Log HTTP requests with Morgan in 'common' format
+app.use(morgan("common"));
+
+// Parse incoming JSON request bodies
+app.use(bodyParser.json());
+
+// Parse URL-encoded request bodies (typically from HTML forms)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Enable Cross-Origin Resource Sharing (CORS) to allow requests from different origins
+app.use(cors());
+
+// routes
+app.use("/dashboard", dashboardRoutes)
+
+// server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
